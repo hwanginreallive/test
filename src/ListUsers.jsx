041bda414b66825d3ff3b8d3e1765d937709fb71
hwanginreallive <hwangineallive@gmail.com>
2022-06-17 14:15:28 +0700

    update sort

--- src/ListUsers.jsx
+++ src/ListUsers.jsx
@@ -2,38 +2,20 @@ import React from 'react'
 import "./listusers.css"
 import { useEffect, useRef, useState } from 'react';
 import userApi from './api/userApi';
-import {AiOutlineSearch ,AiOutlineSortDescending, AiOutlineSortAscending} from "react-icons/ai"
 
-  const title = [
-    {
-      name: 'Full name',
-    },
-    {
-      name: 'Username',
-    },
-    {
-      name: 'Thumnail',
-    },
-  ]
 
-  
+
+
 const ListUsers = () => {
-     
+
   const [listUsers,setListUser] = useState([]) 
-  const [saveListUsers,setSaveListUsers] = useState([]) 
   const [page,setPage]  = useState(1)
-
   const [order,setOrder] = useState("ASC")
-
-  const [result,setResult] = useState({
-    page: page,
-    results: 10,
-  })
-
-
+  const [result,setResult] = useState({page: page, results: 10,})
   const [searchValue,setSearchValue] = useState('')
 
 
+  //Fetch Api
   useEffect(()=>{
     const  fetchUserList = async () =>{
       try {
@@ -43,7 +25,6 @@ const ListUsers = () => {
         }
         const response = await  userApi.getAll(result);
         
-        const saveListUsers = setSaveListUsers(response.results)
 
         setListUser(response.results)
       } catch (error) {
@@ -51,74 +32,93 @@ const ListUsers = () => {
       }
     }
     fetchUserList()
-  },[page])
+  },[page ])
 
-
+  //render list user
   const renderUsers =   (listUsers.filter((val) =>{
         if(searchValue.trim() ==="" ){ 
             return val
         }
         else if(val.login.username.toLowerCase().includes(searchValue.trim().toLowerCase())) {
             return val
-        }
-  } )).map( (user,index) =>{
-        return(
-        <tr className='tr'   key={index} >
-            <td  > {Object.values(user.name).join(' ')}  </td>
-            <td  > {user.login.username}  </td>
-            <td   > <img src={user.picture.thumbnail} alt="" />  </td>
-        </tr>
+        }}))
+        .map( (user,index) =>{
+            return(
+            <tr className='tr'   key={index} >
+                <td  > {Object.values(user.name).join(' ')}  </td>
+                <td  > {user.login.username}  </td>
+                <td   > <img src={user.picture.thumbnail} alt="" />  </td>
+            </tr>
         )
     })
-
+  
+  
+  //next page
   const handleNextPage = () =>{
     setPage(page+1)
     setResult({
         page: page,
         results: 100
-    })
-    handleBackToTop()
+        })
+    handleBackOnTop()
   }
 
+
+  //prev page
   const handlePrevPage = () =>{
      page -1 ===0 ? setPage(page)  :setPage(page-1)
     if(page - 1 ===1) {
         setResult({
         page: page,
         results: 10
-    })
-        
+        })
     }
-        handleBackToTop()
+    handleBackOnTop()
   }
 
-  const handleBackToTop = () =>{
+  //Come back on top
+  const handleBackOnTop = () =>{
         document.body.scrollTop= 0;
         document.documentElement.scrollTop= 0;
   }
  
 
-  const handleSort =(props) =>{
-
+  //Sort by first name
+  const handleSortFirstName =(e) =>{
         if(order=== "ASC"){
-
             const sorted = [...listUsers].sort((a,b)=>
-            a[props].toLowerCase() > b[props].toLowerCase() ? 1 : -1
+            a.name.first.toLowerCase() > b.name.first.toLowerCase() ? 1 : -1
         );
             setListUser(sorted);
             setOrder("DSC");
         }
-        if(order=== "DSC"){
-            const sorted = [...listUsers.results.name.first].sort((a,b)=>
-            a[saveListUsers.values.props].toLowerCase() < b[props].toLowerCase() ? 1 : -1
+        else  if(order=== "DSC"){
+            const sorted = [...listUsers].sort((a,b)=>
+             a.name.first.toLowerCase() < b.name.first.toLowerCase() ? 1 : -1
         );
             setListUser(sorted);
             setOrder("ASC");
         }
-        
-
     } 
       
+
+    // sort by username
+    const handleSortUsername =(e) =>{
+        if(order=== "ASC"){
+            const sorted = [...listUsers].sort((a,b)=>
+            a.login.username.toLowerCase() > b.login.username.toLowerCase() ? 1 : -1
+        );
+            setListUser(sorted);
+            setOrder("DSC");
+        }
+        else  if(order=== "DSC"){
+            const sorted = [...listUsers].sort((a,b)=>
+             a.login.username.toLowerCase() < b.login.username.toLowerCase() ? 1 : -1
+        );
+            setListUser(sorted);
+            setOrder("ASC");
+        }
+    } 
 
 
   return (
@@ -130,20 +130,14 @@ const ListUsers = () => {
                 onChange={(e) =>setSearchValue(e.target.value)}
             />
         </div>
-    
-        <button onClick={() => handleSort("first")}  >
-            <AiOutlineSortAscending></AiOutlineSortAscending>
-        </button   >
-        <button onClick={() => handleSort("username")}  >
-            <AiOutlineSortDescending></AiOutlineSortDescending>
-        </button>
+       
       </div>
        <table className='list' >
             <thead>
               <tr >
-                    <th>Full name</th>
-                    <th>Username</th>
-                    <th>Thumnail</th>
+                    <th onClick={() => handleSortFirstName()}>Full name</th>
+                    <th onClick={() => handleSortUsername()} >UserName</th>
+                    <th>Thumbnail</th>
               </tr>
             </thead>
             <tbody>
@@ -154,7 +148,7 @@ const ListUsers = () => {
           <button onClick={handlePrevPage} >Prev</button>
           <i className='page' >{page}</i>
           <button onClick={handleNextPage} >Next</button>
-          <button onClick={handleBackToTop} >Back To Top</button>
+          <button onClick={handleBackOnTop} >Back On Top</button>
         </div>
 
       
